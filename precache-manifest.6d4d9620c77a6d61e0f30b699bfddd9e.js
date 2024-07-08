6adec6f291fbc4fc0b40f178aaa1bcdb5a1e2976
André <andreegoncalvess@gmail.com>
2024-07-08 14:19:39 -0300

    Updates

---
+++ precache-manifest.6d4d9620c77a6d61e0f30b699bfddd9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef2972988fa93da289110695cb304a0",
+    "url": "/react-personal-site/index.html"
+  },
+  {
+    "revision": "8c9ae24dedb3fb1ed1d1",
+    "url": "/react-personal-site/static/css/main.7df8167a.chunk.css"
+  },
+  {
+    "revision": "701f57c09cd9d5db3176",
+    "url": "/react-personal-site/static/js/2.effb1324.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/react-personal-site/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "8c9ae24dedb3fb1ed1d1",
+    "url": "/react-personal-site/static/js/main.5b566297.chunk.js"
+  },
+  {
+    "revision": "68cbc396ca41a62b10bf",
+    "url": "/react-personal-site/static/js/runtime~main.6ea29253.js"
+  },
+  {
+    "revision": "5741eb840d1a44b267da5707af07ea90",
+    "url": "/react-personal-site/static/media/getFetch.5741eb84.cjs"
+  }
+]);
